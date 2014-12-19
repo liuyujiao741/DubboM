1c043c315d27d79884713ac59c8cc82bd431d655
liuyujiao741 <liuyujiao741@163.com>
2014-12-19 10:40:07 +0800

    Revert "DubboM/src"
    This reverts commit 339bc1938e3f7313a22910318ee73edf60c562a2.

--- src/com/dubbo/test/HelloService.java
+++
@@ -1,5 +0,0 @@
-package com.dubbo.test;
-
-public interface HelloService{
-	String sayHello(String name);
-}
--- src/dubbo/test/dubbo/service/TestService.java
+++
@@ -1,5 +0,0 @@
-package dubbo.test.dubbo.service;
-
-public interface TestService {
-	public void testService(String param);
-}
